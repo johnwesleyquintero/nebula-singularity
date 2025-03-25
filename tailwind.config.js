229833a79d375311dec44c6cb260f972b7c15d7d
John Wesley Quintero <johnwesleyquintero@gmail.com>
2025-03-25 11:47:08 +0800

    Initial commit: Project setup with Next.js, Tailwind, and authentication

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,19 @@ module.exports = {
     },
     extend: {
       colors: {
+        neutral: {
+          50: "rgb(250 250 250)",
+          100: "rgb(245 245 245)",
+          200: "rgb(229 229 229)",
+          300: "rgb(212 212 212)",
+          400: "rgb(163 163 163)",
+          500: "rgb(115 115 115)",
+          600: "rgb(82 82 82)",
+          700: "rgb(64 64 64)",
+          800: "rgb(38 38 38)",
+          900: "rgb(23 23 23)",
+          950: "rgb(10 10 10)"
+        },
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
