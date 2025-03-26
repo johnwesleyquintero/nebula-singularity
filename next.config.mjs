e8d85eadc4c9c33bca28947bb98090d086a58bd2
John Wesley Quintero <johnwesleyquintero@gmail.com>
2025-03-26 11:23:16 +0800

    refactor(next.config): remove swcMinify option
    The swcMinify option was removed from the Next.js configuration as it is no longer necessary. This simplifies the configuration file and aligns with the latest best practices for Next.js projects.

--- next.config.mjs
+++ next.config.mjs
@@ -19,7 +19,7 @@ const nextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60,
   },
-  swcMinify: true,
+
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
