7ee6679b86465ea7e455e4f3ebfdb89bd8aa8e4c
John Wesley Quintero <johnwesleyquintero@gmail.com>
2025-03-28 03:31:40 +0800

    test: add initial test setup and configurations
    Add Jest, Babel, and Testing Library configurations to enable testing in the project. Includes sample test file, Jest setup, and necessary dependencies to support testing React components and TypeScript.

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^.+\\.(css|less|scss)$': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest']
+  },
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!**/.next/**',
+  ],
+  rootDir: '.'
+}
